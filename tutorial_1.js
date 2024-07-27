8f8d42d8509cec0662e8ab2d327f1f710a0f5618
Beserker-356 <jenish.23bcs10016@ms.sst.scaler.com>
2024-07-27 22:48:40 +0530

    Tutorial 1 done

---
+++ tutorial_1.js
@@ -0,0 +1,15 @@
+// Simple code to navigate to google.com and save a screenshot
+
+const puppeteer = require("puppeteer");
+
+(async () => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://google.com");
+    await page.screenshot({path: "google.png"});
+    await browser.close();
+  } catch (error) {
+    console.log(error);
+  }
+})();
